d2f354ce91c0e6e5258b4ed55ddc5aeb8bf17ef1
superneko <supernekocat31@gmail.com>
2022-10-24 20:07:57 +0900

    Adopted GithubAPI

--- jester_test/src/jester_test.nim
+++ jester_test/src/jester_test.nim
@@ -1,16 +1,33 @@
-# nimble build + ./jester_test
+# GithubAPIはhttpsのため、-d:sslオプションを付けてコンパイル
+# nim c -d:ssl -r src/jester_test.nim
 # http://localhost:5000(.devcontainer.jsonでポートフォワード設定済)
 import htmlgen
 import jester
+import httpclient
+import json
+
+type
+  GithubUser = object
+    login: string
+    name: string
+    location: string
+    bio: string
+    public_repos: int
 
 routes:
     get "/":
         resp h1("Hello, Jester!!")
 
-    get "/hello/@name?":
-        # "/hello/".
+    # Github APIからユーザ情報を取得
+    get "/user/@name?":
+        # 指定ナシ
         if @"name" == "":
-            resp p("名前が設定されていません")
-        # "/hello/hogehoge"
+            resp p("ユーザ名が設定されていません")
+        # ex) https://api.github.com/users/superneko160
         else:
-            resp p("Hello, " & @"name")
+            var url = "https://api.github.com/users/" & @"name"
+            var client = newHttpClient()
+            let res: string = client.getContent(url)
+            let resJson: JsonNode = parseJson(res)
+            let data = to(resJson, GithubUser) 
+            resp p(data.login & " " & data.name)
