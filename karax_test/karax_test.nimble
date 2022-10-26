9d8344e9d7e0e54c86c8f841fa1c04db2396c686
superneko <supernekocat31@gmail.com>
2022-10-26 21:27:34 +0900

    add karax_test

---
+++ karax_test/karax_test.nimble
@@ -0,0 +1,12 @@
+# Package
+
+version       = "0.1.0"
+author        = "superneko"
+description   = "karax test"
+license       = "MIT"
+srcDir        = "src"
+
+
+# Dependencies
+
+requires "nim >= 1.6.6"
---
+++ karax_test/src/karax_test.nim
@@ -0,0 +1,9 @@
+# nimble install karax
+# nim js src/karax_test.nim (jsファイルとしてコンパイル)
+include karax/prelude
+
+proc createDom(): VNode =
+  return buildHtml(tdiv):
+    text "Hello, Karax!"
+
+setRenderer createDom
